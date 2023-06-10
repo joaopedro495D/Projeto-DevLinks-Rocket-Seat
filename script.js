7fdd8bb61c1b7fe3a04c7bb3c2028b4d3dadcc59
Your Name~ <jpedro.oliv.cavalc@gmail.com>
2023-06-10 13:11:28 -0300

    inserindo atualizações no projeto

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay")
+const btnCloseModal = document.querySelector(".close-modal")
+const btnOpenModal = document.querySelector(".show-modal")
+console.log(btnOpenModal)
+
 function toggleMode() {
   const html = document.documentElement
-
-  /* if(html.classList.contains('light')){
-    html.classList.remove('light')
-  } else {
-    html.classList.add('light')
-  }*/
   html.classList.toggle("light")
+} 
+
+function openModal() {
+  modal.classList.remove("hidden")
+  overlay.classList.remove("hidden")
 }
+
+function closeModal() {
+  modal.classList.add("hidden")
+  overlay.classList.add("hidden")
+}
+btnOpenModal.addEventListener("click", openModal)
+btnCloseModal.addEventListener("click", closeModal)
+overlay.addEventListener("click", closeModal)
+
